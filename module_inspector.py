68f2afa2d59f77b3b6b76697fc033ab14eb96b15
Joe Wilner <jhwilner1@gmail.com>
2014-06-22 23:51:51 -0400

    Adds directory exploration and CSV export

--- module_inspector.py
+++ module_inspector.py
@@ -1,10 +1,15 @@
+import csv
 from itertools import imap, chain
+import os
 import re
+import sys
 
 IMPORT_REGEX = re.compile(r"""(?:^from\s([\w\.]+)\simport\s.+$)|
-                              (?:^import\s([\w\.]+)\sas.+$)|
+                              (?:^import\s([\w\.]+)\sas\s.+$)|
                               (?:^import(?:\s([\w\.,\s]+)?)$)""",
                           re.VERBOSE)
+
+PYTHON_EXT_REGEX = re.compile(r"\.py$")
 
 
 def parse_line(line):
@@ -52,8 +57,37 @@ def _flat_map(f, *args):
     return chain.from_iterable(imap(f, *args))
 
 
+def _get_all_files(directory):
+    for dirname, _, filenames in os.walk(os.path.abspath(directory)):
+        for filename in filenames:
+            yield os.path.join(dirname, filename)
+
+
+def get_all_imports_in_project(directory, predicate=PYTHON_EXT_REGEX.search):
+    return {filename: list(find_imports_in_file(filename))
+            for filename in (fname for fname in _get_all_files(directory)
+                             if predicate(fname))}
+
+
+def get_module_name_from_filename(full_path):
+    base_name, module_name = os.path.split(full_path)
+    while is_invalid_module_name(module_name):
+        base_name, module_name = os.path.split(base_name)
+    return PYTHON_EXT_REGEX.sub('', module_name)
+
+
+def is_invalid_module_name(module_name):
+    return PYTHON_EXT_REGEX.sub('', module_name) == '__init__'
+
+
+def write_to_csv(target_file, file_imports):
+    with open(target_file, 'w') as f:
+        writer = csv.writer(f)
+        for filename, imports in file_imports.iteritems():
+            writer.writerow([filename, get_module_name_from_filename(filename)]
+                            + imports)
+
+
 if __name__ == '__main__':
-    import sys
-    imports = find_imports_in_file(sys.argv[1])
-    print "The following imports were found in {}:".format(sys.argv[1])
-    print "\n".join(imports)
+    all_imports = get_all_imports_in_project(sys.argv[1])
+    write_to_csv(sys.argv[2], all_imports)
